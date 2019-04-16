8b325ce1dbcdf353284be5af1a9812d7936a49a8
“ne <735834365@qq.com>
2019-04-16 20:59:04 +0800

    少量修改

--- src/test/java/cn/team/service/UserServiceTest.java
+++ src/test/java/cn/team/service/UserServiceTest.java
@@ -2,13 +2,22 @@ package cn.team.service;
 
 
 import cn.team.RightsManageApplicationTest;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 
-class UserServiceTest extends RightsManageApplicationTest {
+import java.util.List;
 
-//    @Autowired
-//    MenuRoleService menuRoleService;
-//    public
+public class UserServiceTest extends RightsManageApplicationTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testGetAllUesr() {
+        List userList = userService.getAllUser();
+        System.out.println(userList);
+    }
+
 
 }
