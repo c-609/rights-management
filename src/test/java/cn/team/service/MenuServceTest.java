64316e24a22c146c9fdd440eca310d15368ea95b
“ne <735834365@qq.com>
2019-04-17 10:08:51 +0800

    修改菜单管理功能返回格式

--- src/test/java/cn/team/service/MenuServceTest.java
+++ src/test/java/cn/team/service/MenuServceTest.java
@@ -2,6 +2,7 @@ package cn.team.service;
 
 import cn.team.RightsManageApplicationTest;
 import cn.team.bean.Menu;
+import cn.team.mapper.MenuMapper;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,7 +16,12 @@ public class MenuServceTest extends RightsManageApplicationTest {
     @Autowired
     private MenuService menuService;
 
+    @Test
+    public void testGetMenuByUId() {
+//        List<Menu> list = menuService.getMenusByUId();
 
+//        System.out.println(list);
+    }
 
     @Test
     public void testAllMenu() {
